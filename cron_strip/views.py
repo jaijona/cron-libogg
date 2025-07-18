c1efad2c11d1a261880abceb80a9e7e006ab8b52
Jonathan <jaijona@gmail.com>
2025-07-17 20:42:30 -0500

    correccion4

--- cron_strip/views.py
+++ cron_strip/views.py
@@ -1,4 +1,5 @@
 # views.py
+"""
 from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
 from decouple import config
@@ -16,3 +17,31 @@ def ejecutar_cron(request):
     except Exception as e:
         return JsonResponse({'error': str(e)}, status=500)
 
+"""
+
+from django.http import JsonResponse
+from decouple import config
+from django.views.decorators.csrf import csrf_exempt
+from cron_strip.utils import guardar_promedios_stripchat
+import logging
+
+logger = logging.getLogger(__name__)
+
+@csrf_exempt
+def ejecutar_cron(request):
+    token = request.GET.get('token') or request.POST.get('token')
+
+    logger.info("🔧 CRON endpoint fue llamado.")
+    logger.info(f"Token recibido: {token}")
+
+    if token != config('CRON_TOKEN'):
+        logger.warning("❌ Token inválido.")
+        return JsonResponse({'error': 'Token inválido'}, status=403)
+
+    try:
+        guardar_promedios_stripchat()
+        logger.info("✅ Cron ejecutado correctamente.")
+        return JsonResponse({'status': 'OK'})
+    except Exception as e:
+        logger.exception("💥 Error al ejecutar guardar_promedios_stripchat")
+        return JsonResponse({'error': str(e)}, status=500)
