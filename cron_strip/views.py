f50dc146580997cead7ebe0f1427bd1f3d28cc5a
Jonathan <jaijona@gmail.com>
2025-07-17 20:34:21 -0500

    correccion2

--- cron_strip/views.py
+++ cron_strip/views.py
@@ -1,7 +1,7 @@
-from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
-from cron_strip.utils import guardar_promedios_stripchat
+from django.http import JsonResponse
 from decouple import config
+from cron_strip.utils import guardar_promedios_stripchat
 
 @csrf_exempt
 def ejecutar_cron(request):
@@ -9,7 +9,12 @@ def ejecutar_cron(request):
     if token != config('CRON_TOKEN'):
         return JsonResponse({'error': 'No autorizado'}, status=403)
 
-    if request.method == 'POST':
-        guardar_promedios_stripchat()
-        return JsonResponse({'status': 'ok'})
+    # Acepta GET y POST
+    if request.method in ['GET', 'POST']:
+        try:
+            guardar_promedios_stripchat()
+            return JsonResponse({'status': 'ok'})
+        except Exception as e:
+            return JsonResponse({'error': str(e)}, status=500)
+
     return JsonResponse({'error': 'Método no permitido'}, status=405)
